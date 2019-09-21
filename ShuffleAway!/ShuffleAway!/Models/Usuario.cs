82d1d41c3a911626dd9b0cfa9b0118926f0265ed
nachomaldo778 <55333088+nachomaldo778@users.noreply.github.com>
2019-09-21 19:39:40 -0300

    Agregando clases

---
+++ ShuffleAway!/ShuffleAway!/Models/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShuffleAway_.Models
+{
+    public class Usuario
+    {
+        public long idUsuario { get; set; }
+        public string nombreUsuario { get; set; }
+        public string pass { get; set; }
+        public long idTipoUsuario { get; set; }
+        public string email { get; set; }
+        public long idPais { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(long idUsuario, string nombreUsuario, string pass, long idTipoUsuario, string email, long pais, string nombre, string apellido, DateTime fechaNacimiento)
+        {
+            this.idUsuario = idUsuario;
+            this.nombreUsuario = nombreUsuario;
+            this.pass = pass;
+            this.idTipoUsuario = idTipoUsuario;
+            this.email = email;
+            this.idPais = pais;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaNacimiento = fechaNacimiento;
+        }
+    }
+}
