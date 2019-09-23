1070a2edb113cceda221207522d5936e773c9730
nachomaldo778 <55333088+nachomaldo778@users.noreply.github.com>
2019-09-22 21:21:29 -0300

    VIEWMODEL

---
+++ ShuffleAway!/ShuffleAway!/Models/MVC/usuarioViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShuffleAway_.Models.MVC
+{
+    public class usuarioViewModel
+    {
+        public Usuario usuario { get; set; }
+    }
+}
