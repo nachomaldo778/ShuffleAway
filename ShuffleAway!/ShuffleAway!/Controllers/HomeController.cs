97dd93d081058553d54a38ee0b906f65bc235763
nachomaldo778 <55333088+nachomaldo778@users.noreply.github.com>
2019-10-03 01:20:47 -0300

    Entrega 3/10 con lista de sorteos activos

--- ShuffleAway!/ShuffleAway!/Controllers/HomeController.cs
+++ ShuffleAway!/ShuffleAway!/Controllers/HomeController.cs
@@ -5,33 +5,19 @@ using System.Web.Mvc;
 
 namespace ShuffleAway_.Controllers
 {
-	public class HomeController : Controller
-	{
-		private static AccesoDatos datos = new AccesoDatos();
+    public class HomeController : Controller
+    {
+        private static AccesoDatos datos = new AccesoDatos();
 
-		public List<Provincia> getListProvincias()
-		{
-			return datos.getListaProvincias();
-		}
-		public ActionResult Index()
-		{
-			MvcModel mvc = new MvcModel();
-			mvc.lstProvincias = getListProvincias();
-			return View(mvc);
-		}
-
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
-		}
-	}
+        public List<Provincia> getListProvincias()
+        {
+            return datos.getListaProvincias();
+        }
+        public ActionResult Index()
+        {
+            MvcModel mvc = new MvcModel();
+            mvc.lstProvincias = getListProvincias();
+            return View(mvc);
+        }
+    }
 }
